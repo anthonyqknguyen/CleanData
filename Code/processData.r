432f075aa087cf3c294316b93fc5de79f7631635
Pranav Kannan <pranavkannan52@gmail.com>
2022-09-26 15:23:27 -0400

    Added comments

--- Code/processData.r
+++ Code/processData.r
@@ -1,5 +1,14 @@
+# Setting woking directory
 setwd(getwd())
+
+# Setting the data directory
 Data_wd <- "../Data/"
+
+# Reading in the counts table
 counts <- read.table(paste(Data_wd, "counts.txt", sep=""), sep="\t")
+
+# Reading in the metadata
 metadata <- read.table(paste(Data_wd, "metadata.txt", sep=""), sep="\t")
+
+# Reading in the taxa data
 taxa <- read.table(paste(Data_wd, "taxa.txt", sep=""), sep="\t")
