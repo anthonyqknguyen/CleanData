3eb63a3111cbcfd4a8cf1afe448767899cdbed3f
anthonyqknguyen <anthonyqn@vt.edu>
2022-10-07 13:29:03 -0400

    read in other data files

--- Code/processData.r
+++ Code/processData.r
@@ -8,8 +8,17 @@ Data_wd <- "./Data/"
 # Reading in the counts table
 counts <- read.table(paste(Data_wd, "counts.txt", sep=""), sep="\t")
 
-# Reading in the metadata
-metadata <- read.table(paste(Data_wd, "metadata.txt", sep=""), sep="\t", fill = TRUE)
+# Reading in the citizen metadata
+citizen_metadata <- read.table(paste(Data_wd, "metadata.txt", sep=""), sep="\t", fill = TRUE)
 
 # Reading in the taxa data
 taxa <- read.table(paste(Data_wd, "taxa.txt", sep=""), sep="\t")
+
+# Reading in Khier data
+khier <- read.table(paste(Data_wd, "khier_2020_fixed.txt", sep=""), sep="\t")
+
+# Reading in ko_noz data
+ko_noz <- read.table(paste(Data_wd, "ko_noz_rn_tmm.txt", sep=""), sep="\t")
+
+# Reading in snf2 metadata
+snf2_metadata <- read.table(paste(Data_wd, "metadata (1).txt", sep=""), sep="\t", fill = TRUE)
