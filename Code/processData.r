1c395d410c78179dffdd661fd67b081e4236d078
anthonyqknguyen <anthonyqn@vt.edu>
2022-10-19 07:12:31 -0400

    made function to create svg hist plots of data

--- Code/processData.r
+++ Code/processData.r
@@ -1,6 +1,6 @@
 # Setting woking directory
 # assumes starting location in terminal is /CleanData
-setwd(getwd())
+setwd("C:/Users/antho/OneDrive - Virginia Tech/School/Year 4/Fall_2022/CMDA_4864/CleanData")
 
 # Setting the data directory
 Data_wd <- "./Data/"
@@ -22,3 +22,29 @@ ko_noz <- read.table(paste(Data_wd, "ko_noz_rn_tmm.txt", sep=""), sep="\t")
 
 # Reading in snf2 metadata
 snf2_metadata <- read.table(paste(Data_wd, "metadata (1).txt", sep=""), sep="\t", fill = TRUE)
+
+
+library(dplyr)
+
+make_hist <- function(data, title= "untitled", save= FALSE, file_name= "untitled.svg") {
+  hist.data <- hist(as.numeric(do.call('c', data)), plot=FALSE)
+  hist.data$counts = log10(hist.data$counts + 1)
+  if (save) {
+    svg(file_name)
+  }
+  plot(hist.data,
+       xlab="Counts", ylab="Frequency (log10)", main=title, 
+       col="lightblue")
+  if (save) {
+    dev.off()
+  }
+}
+
+counts_num <- select(counts, -V1)
+ko_num <- select(ko_noz, -ko)
+ko_num_before <- ko_num[,1:6]
+ko_num_after <- ko_num[,7:12]
+
+make_hist(counts_num, "Citizens Data", TRUE, "Citizens_hist.svg")
+make_hist(ko_num_before, "SnF2 Before Treatment Data", TRUE, "snf2_before_hist.svg")
+make_hist(ko_num_after, "SnF2 After Treatment Data", TRUE, "snf2_after_hist.svg")
